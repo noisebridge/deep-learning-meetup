17e62284286ca61682de393f80ad81e0227cd247
tmelanson17 <tmelanson17@gmail.com>
2021-12-14 23:48:06 -0800

    Added Tensorboard support for images

--- kaggle/kaggle_demo.py
+++ kaggle/kaggle_demo.py
@@ -10,13 +10,13 @@ import cv2
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt 
-from sklearn.model_selection import train_test_split 
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 import torch
 import torchvision
 from torchvision.transforms import ToPILImage
-from torch.nn import DataParallel 
+from torch.nn import DataParallel
 from torch.utils.data import DataLoader
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
@@ -64,14 +64,14 @@ MIN_SCORE = 0.59
 WIDTH = 704
 HEIGHT = 520
 
-# Changes the confidence required for a pixel to be kept for a mask. 
+# Changes the confidence required for a pixel to be kept for a mask.
 # Only used 0.5 till now.
 MASK_THRESHOLD = 0.5
 # =============================
 
 df_train = pd.read_csv(TRAIN_CSV, nrows=5000 if TEST else None)
 ds_train = CellDataset(TRAIN_PATH, df_train, resize=False, transforms=get_transform(train=True))
-dl_train = DataLoader(ds_train, batch_size=BATCH_SIZE, shuffle=True, 
+dl_train = DataLoader(ds_train, batch_size=BATCH_SIZE, shuffle=True,
                       num_workers=2, collate_fn=lambda x: tuple(zip(*x)))
 ds_test = CellTestDataset(TEST_PATH, transforms=get_transform(train=False))
 dl_test = DataLoader(ds_test, shuffle=False, collate_fn=lambda x: tuple(zip(*x)))
@@ -80,7 +80,7 @@ dl_test = DataLoader(ds_test, shuffle=False, collate_fn=lambda x: tuple(zip(*x))
 model = get_model()
 for param in model.parameters():
     param.requires_grad = True
-    
+
 model.train();
 
 # Writer will output to ./runs/ directory by default
@@ -97,7 +97,7 @@ n_batches = len(dl_train)
 
 for epoch in range(1, NUM_EPOCHS + 1):
     print(f"Starting epoch {epoch} of {NUM_EPOCHS}")
-    
+
     time_start = time.time()
     loss_accum = 0.0
     loss_mask_accum = 0.0
@@ -105,23 +105,19 @@ for epoch in range(1, NUM_EPOCHS + 1):
     for img_num, (img_tuple, image_id_tuple) in enumerate(dl_test, 1):
         img = img_tuple[0]
         # Need to index because DataLoader iterates by tuples rather than individually (to support batches).
-        writer.add_image('test_input_images', img.to(DEVICE), img_num)
+        writer.add_image('test_input_images_{}'.format(img_num), img.to(DEVICE), epoch)
         model.eval()
         with torch.no_grad():
             preds = model([img.to(DEVICE)])[0]
-        # Don't think we need any of this, as we're writing a tensor. Keeping in case I'm wrong.
-        # all_preds_masks = np.zeros((HEIGHT, WIDTH))
-        # for mask in preds['masks'].cpu().detach().numpy():
-        #     all_preds_masks = np.logical_or(all_preds_masks, mask[0] > MASK_THRESHOLD)
 
         all_detections = torch.zeros((1, HEIGHT, WIDTH))
         for mask in preds['masks'].cpu().detach():
             all_detections = torch.logical_or(all_detections, mask > MASK_THRESHOLD)
-        writer.add_image('test_prediction', all_detections, img_num)
+        writer.add_image('test_prediction_{}'.format(img_num), all_detections, epoch)
         model.train()
-    
+
     for batch_idx, (images, targets) in enumerate(dl_train, 1):
-    
+
         # Predict
         images = list(image.to(DEVICE) for image in images)
         targets = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
@@ -129,12 +125,12 @@ for epoch in range(1, NUM_EPOCHS + 1):
         # loss_dict = model(images, targets)
         loss_dict = parallel_net(images, targets)
         loss = sum(loss for loss in loss_dict.values())
-        
+
         # Backprop
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
+
         # Logging
         loss_mask = loss_dict['loss_mask'].item()
         loss_accum += loss.item()
@@ -145,23 +141,23 @@ for epoch in range(1, NUM_EPOCHS + 1):
         current_index = (epoch-1)*n_batches + batch_idx
         writer.add_scalar('Train_loss', loss.item(), current_index)
         writer.add_scalar('Train_mask_only_loss', loss_mask, current_index)
-        
+
         if batch_idx % 10 == 1:
             print(f"    [Batch {batch_idx:3d} / {n_batches:3d}] Batch train loss: {loss.item():7.3f}. Mask-only loss: {loss_mask:7.3f}")
-    
+
 
     if USE_SCHEDULER:
         lr_scheduler.step()
-    
+
     # Train losses
     train_loss = loss_accum / n_batches
     train_loss_mask = loss_mask_accum / n_batches
-    
-    
+
+
     elapsed = time.time() - time_start
 
-    
-    
+
+
     torch.save(model.state_dict(), f"pytorch_model-e{epoch}.bin")
     prefix = f"[Epoch {epoch:2d} / {NUM_EPOCHS:2d}]"
     print(f"{prefix} Train mask-only loss: {train_loss_mask:7.3f}")
@@ -169,5 +165,4 @@ for epoch in range(1, NUM_EPOCHS + 1):
 #### ------------------------------------------------------------------------------------------------
 
 writer.close()
-
 
