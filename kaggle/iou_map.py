8c7736fe7dfcfe0e17ed513df2939abd2363e951
Ryan Lewis <rlewis94@gmail.com>
2021-12-29 16:15:19 -0800

    added iou map functions, no changes to main code

---
+++ kaggle/iou_map.py
@@ -0,0 +1,125 @@
+"""
+from https://www.kaggle.com/theoviel/competition-metric-map-iou
+"""
+
+import numpy as np
+
+def rles_to_mask(encs, shape):
+    """
+    Decodes a rle.
+
+    Args:
+        encs (list of str): Rles for each class.
+        shape (tuple [2]): Mask size.
+
+    Returns:
+        np array [shape]: Mask.
+    """
+    img = np.zeros(shape[0] * shape[1], dtype=np.uint)
+    for m, enc in enumerate(encs):
+        if isinstance(enc, np.float) and np.isnan(enc):
+            continue
+        enc_split = enc.split()
+        for i in range(len(enc_split) // 2):
+            start = int(enc_split[2 * i]) - 1
+            length = int(enc_split[2 * i + 1])
+            img[start: start + length] = 1 + m
+    return img.reshape(shape)
+
+
+def compute_iou(labels, y_pred):
+    """
+    Computes the IoU for instance labels and predictions.
+
+    Args:
+        labels (np array): Labels.
+        y_pred (np array): predictions
+
+    Returns:
+        np array: IoU matrix, of size true_objects x pred_objects.
+    """
+
+    true_objects = len(np.unique(labels))
+    pred_objects = len(np.unique(y_pred))
+
+    # Compute intersection between all objects
+    intersection = np.histogram2d(
+        labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects)
+    )[0]
+
+    # Compute areas (needed for finding the union between all objects)
+    area_true = np.histogram(labels, bins=true_objects)[0]
+    area_pred = np.histogram(y_pred, bins=pred_objects)[0]
+    area_true = np.expand_dims(area_true, -1)
+    area_pred = np.expand_dims(area_pred, 0)
+
+    # Compute union
+    union = area_true + area_pred - intersection
+    iou = intersection / union
+
+    return iou[1:, 1:]  # exclude background
+
+def precision_at(threshold, iou):
+    """
+    Computes the precision at a given threshold.
+
+    Args:
+        threshold (float): Threshold.
+        iou (np array [n_truths x n_preds]): IoU matrix.
+
+    Returns:
+        int: Number of true positives,
+        int: Number of false positives,
+        int: Number of false negatives.
+    """
+    matches = iou > threshold
+    true_positives = np.sum(matches, axis=1) >= 1  # Correct objects
+    false_negatives = np.sum(matches, axis=1) == 0  # Missed objects
+    false_positives = np.sum(matches, axis=0) == 0  # Extra objects
+    tp, fp, fn = (
+        np.sum(true_positives),
+        np.sum(false_positives),
+        np.sum(false_negatives),
+    )
+    return tp, fp, fn
+
+def iou_map(truths, preds, verbose=0):
+    """
+    Computes the metric for the competition.
+    Masks contain the segmented pixels where each object has one value associated,
+    and 0 is the background.
+
+    Args:
+        truths (list of masks): Ground truths.
+        preds (list of masks): Predictions.
+        verbose (int, optional): Whether to print infos. Defaults to 0.
+
+    Returns:
+        float: mAP.
+    """
+    ious = [compute_iou(truth, pred) for truth, pred in zip(truths, preds)]
+
+    print(ious[0].shape)
+
+    if verbose:
+        print("Thresh\tTP\tFP\tFN\tPrec.")
+
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        tps, fps, fns = 0, 0, 0
+        for iou in ious:
+            tp, fp, fn = precision_at(t, iou)
+            tps += tp
+            fps += fp
+            fns += fn
+
+        p = tps / (tps + fps + fns)
+        prec.append(p)
+
+        if verbose:
+            print("{:1.3f}\t{}\t{}\t{}\t{:1.3f}".format(t, tps, fps, fns, p))
+
+    if verbose:
+        print("AP\t-\t-\t-\t{:1.3f}".format(np.mean(prec)))
+
+    return np.mean(prec)
