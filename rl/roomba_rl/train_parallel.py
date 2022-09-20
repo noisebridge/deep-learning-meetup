13d8d745f551c9beaa58446dea91f8a29bba943d
tmelanson17 <tmelanson17@gmail.com>
2022-09-20 01:19:00 -0700

    Added a queue

--- rl/roomba_rl/train_parallel.py
+++ rl/roomba_rl/train_parallel.py
@@ -1,15 +1,23 @@
 import numpy as np
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Queue
 import time
+from typing import NamedTuple
+
+class State(NamedTuple):
+    reward: int
+    state: np.array
+    next_state: np.array
+    action: int
 
 
-def training_step(env_list, i,  t, n_actions): 
+def training_step(state_queue, env_list, i,  t, n_actions): 
     env = env_list[i]
     state = env.reset()
     # Start with random crawl
     #epsilon = self._initial_epsilon - (self._initial_epsilon - self._final_epsilon) * (t-self._replay_start_size) / self._final_exploration_count
     #if t < replay_start_size or np.random.rand() < epsilon:
     j=0
+    in_method_queue = list()
     while not env.terminated:
         if True:
             action = np.random.randint(n_actions)
@@ -18,9 +26,18 @@ def training_step(env_list, i,  t, n_actions):
             action = np.argmax(result[0])
         # Take step
         next_state, reward, terminal_state, _ = env.step(action)
+        queue_state = State(
+                reward=reward,
+                state=state,
+                next_state=next_state,
+                action=action
+        )
+        in_method_queue.append(queue_state)
+        state = next_state
         j+=1
     print(i)
     print(f"N actions: {j}")
+    state_queue.put(in_method_queue)
 
 class Trainer:
     def __init__(self, n_processes, n_actions, env_factory):
@@ -34,7 +51,8 @@ class Trainer:
 
     def start_training_step(self, t):
         print("starting parallel step")
-        self._processes = [Process(target=training_step, args=(self._envs, i, t, self._n_actions)) for i in range(self._n_processes)]
+        queue = Queue()
+        self._processes = [Process(target=training_step, args=(queue, self._envs, i, t, self._n_actions)) for i in range(self._n_processes)]
         for p in self._processes:
             p.start()
         for p in self._processes:
@@ -49,8 +67,8 @@ if __name__ == "__main__":
     def roomba_env_factory():
         return RoombaEnv(max_episode_steps=1000)
     tic = time.perf_counter()
-    n_proc=8
-    games=5
+    n_proc=4
+    games=10
     trainer = Trainer(n_processes=n_proc, n_actions=4, env_factory=roomba_env_factory)
     for i in range(games):
         trainer.start_training_step(i)
